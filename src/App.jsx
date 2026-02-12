2026de5ff4758f3a99e1581c17c0de287ee8dfa5
Connor Francis <connorfrancis@live.com>
2026-02-12 12:53:52 -1000

    done, with some assistance

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Layout from "./layout/Layout.jsx";
+import { Routes, Route } from "react-router";
+
+import Layout from "./layout/Layout";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
 import ActivitiesPage from "./activities/ActivitiesPage";
+import ActivityDetails from "./activities/ActivityDetails";
+import RoutinesPage from "./routines/RoutinesPage";
+import RoutineDetails from "./routines/RoutineDetails";
 import Error404 from "./Error404.jsx";
-import { Route, Routes } from "react-router";
 
 /**
  * Fitness Trackr is a platform where fitness enthusiasts can share their workouts and
@@ -11,15 +15,18 @@ import { Route, Routes } from "react-router";
  * account will be able to upload and manage their own activities.
  */
 export default function App() {
-  return(
+  return (
     <Routes>
-      <Route element={<Layout/>}>
-        <Route path='/' element={<Login/>}></Route>
-        <Route path='/register' element={<Register/>}></Route>
-        <Route path = '/login' element={<Login/>}></Route>
-        <Route path = '/activities' element={<ActivitiesPage/>}></Route>
-        <Route path = '*' element = {<Error404/>}></Route>
+      <Route element={<Layout />}>
+        <Route index element={<ActivitiesPage />} />
+        <Route path="/activities" element={<ActivitiesPage />} />
+        <Route path="/activities/:id" element={<ActivityDetails />} />
+        <Route path="/routines" element={<RoutinesPage />} />
+        <Route path="/routines/:id" element={<RoutineDetails />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error404 />} />
       </Route>
     </Routes>
-  )
+  );
 }
--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,27 +1,22 @@
-import { NavLink, useNavigate } from "react-router";
+import { NavLink } from "react-router";
 import { useAuth } from "../auth/AuthContext";
-
 
 /** Navbar with site navigation links */
 export default function Navbar() {
   const { token, logout } = useAuth();
-  const navigate = useNavigate()
-  
-  function handleLogout(){
-    logout();
-    navigate('/activities')
-  }
   return (
     <header>
       <p>Fitness Trackr</p>
       <nav>
-        <NavLink to ='/activities'>Activities</NavLink>
+        <NavLink to="/activities">Activities</NavLink>
+        <NavLink to="/routines">Routines</NavLink>
+        {/* This remains <a> because it's not a real link! */}
         {token ? (
-          <a onClick={handleLogout}>Log out</a>
+          <a onClick={() => logout()}>Log out</a>
         ) : (
           <>
-            <NavLink to ='/register'>Register</NavLink>
-            <NavLink to='/login'>Login</NavLink>
+            <NavLink to="/register">Register</NavLink>
+            <NavLink to="/login">Login</NavLink>
           </>
         )}
       </nav>
